e978013633c49995103e5467dcd168a600a6d4a6
recodeleadership <recodeleadership@gmail.com>
2025-11-24 21:47:13 +0000

    Fix indicators to be outside of box and different alignment issues

--- assets/js/testimonial-equalizer.js
+++ assets/js/testimonial-equalizer.js
@@ -4,26 +4,17 @@ document.addEventListener("DOMContentLoaded", function () {
     let maxHeight = 0;
 
     // Reset first
-    items.forEach(item => {
-      item.style.minHeight = "0px";
-    });
+    items.forEach(item => { item.style.minHeight = "0px"; });
 
     // Find tallest
-    items.forEach(item => {
-      const h = item.scrollHeight;
-      if (h > maxHeight) maxHeight = h;
-    });
+    items.forEach(item => { if (item.scrollHeight > maxHeight) maxHeight = item.scrollHeight; });
 
     // Apply height to all
-    items.forEach(item => {
-      item.style.minHeight = maxHeight + "px";
-    });
+    items.forEach(item => { item.style.minHeight = maxHeight + "px"; });
   }
 
-  // On load
   equalizeHeights();
 
-  // On resize (throttled)
   let resizeTimer;
   window.addEventListener("resize", function () {
     clearTimeout(resizeTimer);
